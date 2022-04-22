c2ceb70c441f30ba6aadfbe9896bbb633554f596
AlishaMomin <am05757@st.habib.edu.pk>
2022-04-22 22:10:10 +0500

    listing html,views,url updated

--- amalpro/mainapp/views.py
+++ amalpro/mainapp/views.py
@@ -13,8 +13,6 @@ def dashboard(request):
 def test(request):
     return render(request, "test.html")
 
-
-
 def customers(request):
     form = CustomerForm()
     customers = Customer.objects.all()
@@ -33,6 +31,7 @@ def serviceprovider(request):
     SP = ServiceProviders.objects.all()
 
     return render(request, "serviceprovider.html", {"form": form, "serviceproviders": SP})
+
 def booking(request):
     form = BookingForm()
     bookings = Booking.objects.all()
@@ -122,6 +121,38 @@ def postBooking(request):
     if request.accepts and request.method == "PUT":
         # get the form data
         form = BookingForm(request.POST)
+        # save the data and after fetch the object in instance
+        if form.is_valid():
+            instance = form.save()
+            ser_instance = serializers.serialize('json', [ instance, ])
+            # send to client side.
+            return JsonResponse({"instance": ser_instance}, status=200)
+        else:
+            # some form errors occured.
+            return JsonResponse({"error": form.errors}, status=400)
+
+    # some error occured
+    return JsonResponse({"error": ""}, status=400)
+
+def postListing(request):
+    # request should be ajax and method should be POST.
+    if request.accepts and request.method == "POST":
+        # get the form data
+        form = ListingForm(request.POST)
+        # save the data and after fetch the object in instance
+        if form.is_valid():
+            instance = form.save()
+            # serialize in new Customer object in json
+            ser_instance = serializers.serialize('json', [ instance, ])
+            # send to client side.
+            return JsonResponse({"instance": ser_instance}, status=200)
+        else:
+            # some form errors occured.
+            return JsonResponse({"error": form.errors}, status=400)
+
+    if request.accepts and request.method == "PUT":
+        # get the form data
+        form = ListingForm(request.POST)
         # save the data and after fetch the object in instance
         if form.is_valid():
             instance = form.save()
