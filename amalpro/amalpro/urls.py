1cc41297d839fdc0496e97d3ef7a47966b4790de
Hammad Maqdoom <hammadmaqdoom1@gmail.com>
2022-04-23 00:37:28 +0500

    add header to admin panel

--- amalpro/amalpro/urls.py
+++ amalpro/amalpro/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Amal Admin"
+admin.site.site_title = "Amal Admin Portal"
+admin.site.index_title = "Welcome to Amal Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
