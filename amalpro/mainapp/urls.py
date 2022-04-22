36b07c995a5bbedcf0b40ea1835a9df23caa6263
AlishaMomin <am05757@st.habib.edu.pk>
2022-04-23 00:07:08 +0500

    Password reset Completed

--- amalpro/mainapp/urls.py
+++ amalpro/mainapp/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views 
 from . import views
+
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
@@ -10,6 +12,7 @@ urlpatterns = [
     path("register", views.register_request, name="register"),
     path("logout", views.logout_request, name= "logout"),
     path("login", views.login_request, name="login"),
+    path("password_reset", views.password_reset_request, name="password_reset"),
 
     path('customers/', views.customers, name='customers'),
     path('api/customer', views.postCustomer, name = "post_customer"),
@@ -23,6 +26,10 @@ urlpatterns = [
     path('serviceprovider/', views.serviceprovider, name='serviceprovider'),
     path('api/sp', views.postServiceprovider, name = "post_sp"),
     
-    path('test/', views.test, name = "test")
+    path('test/', views.test, name = "test"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),      
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
